ca0a3886186b61cdebf80a237020e050f5316d27
Faraz Ali <faraz@Farazs-MacBook-Pro.local>
2020-07-27 22:38:30 -0400

    Change filter to Chebyshev Type II + Linear band spacing

--- generate_bandpass_filters.m
+++ generate_bandpass_filters.m
@@ -3,7 +3,11 @@ function bandpass_filters = generate_bandpass_filters(frequency_bands, sampling_
     for num = 1 : length(frequency_bands)
         band = frequency_bands{num};
         normalized_band = band/(sampling_rate/2);
-        [zeros, poles, gain] = butter(2*filter_order, normalized_band);
+        
+        % 1st Prototype - Butterworth Filter
+        % [zeros, poles, gain] = butter(2*filter_order, normalized_band);   
+        % 2nd Prototype - Chebyshev Type II Filter
+        [zeros, poles, gain] = cheby2(filter_order/2, 20, normalized_band);
         [sos, gain] = zp2sos(zeros, poles, gain);
         filter_cell = {sos, gain};
         bandpass_filters{num, 1} = filter_cell;
